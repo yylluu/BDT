5bd7e4308b48ed8b60abb6ba80ae8aba2d8bbe1d
maxilbert <11216294+yylluu@users.noreply.github.com>
2022-09-02 21:12:03 +0800

    Update run_local_network_test.sh

--- run_local_network_test.sh
+++ run_local_network_test.sh
@@ -9,6 +9,6 @@ llall python3
 i=0
 while [ "$i" -lt $1 ]; do
     echo "start node $i..."
-    python3 run_socket_node.py --sid 'sidA' --id $i --N $1 --f $2 --B $3 --K $4 --S 50 --T 1 --F 1000000 --P "bdt" &
+    python3 run_socket_node.py --sid 'sidA' --id $i --N $1 --f $2 --B $3 --K $4 --S 50 --T 1 --F 1000000 --P "dumbo" &
     i=$(( i + 1 ))
 done 
